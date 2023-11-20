852ccd989b4aeb0bde510c517ea9829753343f9d
Максим <105449753+savamaxxx@users.noreply.github.com>
2023-11-20 15:32:50 +0300

    a1

---
+++ src/pages/one.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+const One = () => {
+    return (
+        <div>
+            One page
+            <br />
+            <Link href="/">Main</Link>
+        </div>
+    );
+};
+
+export default One;
---
+++ src/pages/two.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+const Two = () => {
+    return (
+        <div>
+            Two page
+            <br />
+            <Link href="/">Main</Link>
+        </div>
+    );
+};
+
+export default Two;
